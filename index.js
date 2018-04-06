4fb5e23f662dd72a290cb577eb19adf52b4bf25b
Ryan Sherrill <ryanbsherrill@gmail.com>
2018-04-06 13:27:34 -0400

    getting and logging data from API

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const request = require('request');
+require('dotenv').config();
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+request.get(`http://api.eia.gov/series/?api_key=${process.env.EIA_API_KEY}&series_id=TOTAL.RETCBUS.A`, (err, res, body) => {
+  if(err) {
+    return console.log(err);
+  }
+  // console.log('body: ' + body);
+  var jsonObj = JSON.parse(body);
+
+  for (let stat of jsonObj['series'][0].data) {
+    console.log(stat);
+  }
+  // console.log(jsonObj['series'][0].data[10]);
 });
 
 const PORT = process.env.PORT || 8080;
